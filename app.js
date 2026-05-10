3eb1a892985891f1e17c02ea6a16a28bd9120443
fmesclassroomrequests-cyber <fmesclassroomrequests@gmail.com>
2026-05-10 19:02:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ chatForm.addEventListener("submit", async (e) => {
 
         const title = document.createElement("div");
         title.className = "title";
-        title.textContent =  ${req.room} – ${req.category}`;
+        title.textContent =  `${req.room} – ${req.category}`;
        
         const subtitle = document.createElement("div");
         subtitle.className = "subtitle";
